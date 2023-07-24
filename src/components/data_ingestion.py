0901fdd63972723572667732230e37fefb2c2ebb
Anujdh8755 <anujdhyani232@gmail.com>
2023-07-24 14:54:04 +0530

    web page templates

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -53,7 +53,7 @@ class Dataingestion :
 
 if __name__=="__main__":
     obj = Dataingestion()
-    train_data , test_data =obj.initiate_data_ingestion()
+    obj.initiate_data_ingestion()
     
-    data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    # data_transformation = DataTransformation()
+    # data_transformation.initiate_data_transformation(train_data, test_data)
